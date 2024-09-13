201f31135ea9f443e88751a6c3d5d2c1c6b912be
Felipe V. Sobral <xfelipesobral@gmail.com>
2024-09-13 14:45:16 -0300

    Update package.json version to 1.0.11

---
+++ src/sdtmarcasg.d.ts
@@ -0,0 +1,10 @@
+export interface GxsdtMarcaSG {
+    MarcaCod: number;
+    MarcaNome: string;
+    MarcaLogo: string;
+    MarcaisCorte: boolean;
+    MarcaLogoPeq: string;
+    DivisaoLoteCod: number;
+    MarcaFinalizaCorteExpedicao: boolean;
+}
+        
