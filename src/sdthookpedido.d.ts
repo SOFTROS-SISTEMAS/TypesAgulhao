3c4f7a6c05c3c4eb1767731cb1953f600916e8a4
Felipe V. Sobral <xfelipesobral@gmail.com>
2025-02-07 11:16:23 -0300

    sdthookpedido

--- src/sdthookpedido.d.ts
+++ src/sdthookpedido.d.ts
@@ -1,61 +1,59 @@
-
 export interface GxsdtHookPedidocapa {
-    PedidoId: number;
-    PedidoVendedorNome: string;
-    PedidoClienteNome: string;
-    PedidoDataEmissao: string;
-    PedidoValorLiquido: number;
-    PedidoValorBruto: number;
-    PedidoValorPago: number;
-    PedidoRestaAinda: number;
-    PedidoTroco: number;
-    PedidoTotalItens: number;
-    PedidoRetorno: number;
-    PedidoDescontoPerc: number;
-    PedidoDescontoDinheiro: number;
+	PedidoId: number;
+	PedidoVendedorNome: number;
+	PedidoClienteNome: number;
+	PedidoDataEmissao: string;
+	PedidoValorLiquido: number;
+	PedidoValorBruto: number;
+	PedidoValorPago: number;
+	PedidoRestaAinda: number;
+	PedidoTroco: number;
+	PedidoTotalItens: number;
+	PedidoRetorno: number;
+	PedidoDescontoPerc: number;
+	PedidoDescontoDinheiro: number;
 }
-        
+
 export interface GxsdtHookPedidoitens {
-    PedidoItemId: number;
-    ProdutoGradeBarras: number;
-    ProdutoReferencia: string;
-    ProdutoDescricao: string;
-    ProdutoGradeNumero: string;
-    PedidoItemValorUnitario: number;
-    PedidoItemDesconto: number;
-    PedidoItemQuantidade: number;
-    PedidoItemValorLiquido: number;
-    PedidoItemValorTabela: number;
-    ProdutoGradeCorNome: string;
+	PedidoItemId: number;
+	ProdutoGradeBarras: number;
+	ProdutoReferencia: string;
+	ProdutoDescricao: string;
+	ProdutoGradeNumero: string;
+	PedidoItemValorUnitario: number;
+	PedidoItemDesconto: number;
+	PedidoItemQuantidade: number;
+	PedidoItemValorLiquido: number;
+	PedidoItemValorTabela: number;
+	ProdutoGradeCorNome: number;
 }
-        
+
 export interface GxsdtHookPedidopagamentostef {
-    transacaoTEFStatus: string;
-    transacaoTEFValor: number;
-    transacaoTEFData: string;
+	transacaoTEFStatus: string;
+	transacaoTEFValor: number;
+	transacaoTEFData: string;
 }
-        
+
 export interface GxsdtHookPedidopagamentospix {
-    PixCopiaCola: string;
-    PixValor: number;
-    PixDesc: string;
-    PixSts: string;
-    PixDataHora: string;
+	PixCopiaCola: string;
+	PixValor: number;
+	PixDesc: string;
+	PixSts: string;
+	PixDataHora: string;
 }
-        
+
 export interface GxsdtHookPedidopagamentos {
-    FormaPagamentoCod: number;
-    FormaPagamentoValor: number;
-    tef: GxsdtHookPedidopagamentostef;
-    pix: GxsdtHookPedidopagamentospix;
+	FormaPagamentoCod: number;
+	FormaPagamentoValor: number;
+	tef: GxsdtHookPedidopagamentostef;
+	pix: GxsdtHookPedidopagamentospix;
 }
-        
+
 export interface GxsdtHookPedido {
-    id: number;
-    empresa: number;
-    vendedor: number;
-    capa: GxsdtHookPedidocapa;
-    itens: GxsdtHookPedidoitens[];
-    pagamentos: GxsdtHookPedidopagamentos[];
+	id: number;
+	empresa: number;
+	vendedor: number;
+	capa: GxsdtHookPedidocapa;
+	itens: GxsdtHookPedidoitens[];
+	pagamentos: GxsdtHookPedidopagamentos[];
 }
-        
