26b67c09afd0aa3f13b4012e48910dc2a2ab567b
Felipe V. Sobral <xfelipesobral@gmail.com>
2025-02-19 11:56:04 -0300

    atualizacao api integracoes

--- src/sdtapiintegracoes.d.ts
+++ src/sdtapiintegracoes.d.ts
@@ -2,17 +2,73 @@ export interface GxsdtApiIntegracoeserros {
 	mensagem: string;
 	tecnico: string;
 	produtoCod: number;
+	integracaoCod: number;
 }
 
 export interface GxsdtApiIntegracoessucessos {
 	mensagem: string;
 	tecnico: string;
 	produtoCod: number;
+	integracaoCod: number;
+}
+
+export interface GxsdtApiIntegracoesintegracoes {
+	integracaoCod: number;
+	filialCod: number;
+	configuracoes: string;
+}
+
+export interface GxsdtApiIntegracoesprodutosimagens {
+	produtoImagemCodigo: number;
+	produtoImagemOrdem: number | null;
+	produtoImagemUrlExterna: string;
+	produtoImagemCorCod: number;
+}
+
+export interface GxsdtApiIntegracoesprodutosgradesintegracoes {
+	integracaoCod: number;
+	estoque: number;
+	precoPromocional: number;
+	preco: number;
+}
+
+export interface GxsdtApiIntegracoesprodutosgrades {
+	produtoGradeNumero: string;
+	produtoGradeId: number;
+	produtoGradeBarras: number;
+	corNome: string;
+	corCod: number;
+	corHexa: string;
+	corImgExt: string | null;
+	preco: number;
+	integracoes: GxsdtApiIntegracoesprodutosgradesintegracoes[];
+}
+
+export interface GxsdtApiIntegracoesprodutos {
+	produtoCodigo: number;
+	produtoEcomNome: string;
+	produtoEcomDesc: string;
+	produtoNomeAbreviado: string;
+	produtoReferencia: string;
+	produtoDescricao: string;
+	produtoSexo: string;
+	marcaNome: string;
+	produtoAltura: number;
+	produtoLargura: number;
+	produtoComprimento: number;
+	produtoPeso: number;
+	unidadeMedidaSigla: string;
+	imagens: GxsdtApiIntegracoesprodutosimagens[];
+	grades: GxsdtApiIntegracoesprodutosgrades[];
 }
 
 export interface GxsdtApiIntegracoes {
 	acao: string;
 	coringa: string;
+	filiais: number[];
+	listaCodigosProdutos: number[];
 	erros: GxsdtApiIntegracoeserros[];
 	sucessos: GxsdtApiIntegracoessucessos[];
+	integracoes: GxsdtApiIntegracoesintegracoes[];
+	produtos: GxsdtApiIntegracoesprodutos[];
 }
